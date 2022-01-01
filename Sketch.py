5982c14f1cb1cf6e5afaa4cac78cec73e3f7b9a5
LeKilleRGallet <arico@unal.edu.co>
2022-01-01 12:27:40 -0500

    Updated: 01/01/2022

--- Sketch.py
+++ Sketch.py
@@ -1,13 +1,21 @@
-def decorate(func):
-    def inner():
-        print("hi")
-        func()
-        print("bye")
-    return inner
+#create a superclass of a rectangle and a subclass of a square
+class Rectangle:
+    def __init__(self,height,width):
+        self.height = height
+        self.width = width
+    def area(self):
+        return self.height * self.width
+    def perimeter(self):
+        return 2 * self.height + 2 * self.width
 
-@decorate
-def test():
-    print("test")
+class Square(Rectangle):
+    def __init__(self,side):
+        super().__init__(side,side)
 
-test()
+square = Square(5)
+print(square.area())
+print(square.perimeter())
 
+rectangle = Rectangle(5,10)
+print(rectangle.area())
+print(rectangle.perimeter())
