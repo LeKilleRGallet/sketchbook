97c3313514ee153f6626877de1cc5f047d26067d
LeKilleRGallet <arico@unal.edu.co>
2022-01-13 22:19:36 -0500

    Updated: 13/01/2022

--- Sketch.py
+++ Sketch.py
@@ -1,162 +1,32 @@
-# # # # # # while True:
-# # # # # #     try:
-# # # # # #         range_min = input('insert floor: ')
-# # # # # #         if range_min == '':
-# # # # # #             range_min = None
-# # # # # #             break
-# # # # # #         else:
-# # # # # #             range_min = int(range_min)
-# # # # # #             break
-# # # # # #     except ValueError:
-# # # # # #         print("Invalid input, just integers or empty, please try again")
+import random
+import numpy as np
 
-# # # # # # while True:
-# # # # # #     try:
-# # # # # #         range_max = input('insert ceil: ')
-# # # # # #         if range_max == '':
-# # # # # #             range_max = None
-# # # # # #             break
-# # # # # #         else:
-# # # # # #             range_max = int(range_max)
-# # # # # #             break
-# # # # # #     except ValueError:
-# # # # # #         print("Invalid input, just integers or empty, please try again")
+sudoku_size = 9
+difficulty_level = 1
 
-# # # # # # print(range_min, range_max)
+# def random_matrix(difficulty, sudoku_size):
+c = [10*difficulty_level]+[1]*sudoku_size
+#     m = []
+#     total_zeros = 0
+#     for _ in range(sudoku_size):
+#         m.append(random.sample([0, *range(1,sudoku_size+1)], counts = c, k = sudoku_size))
+#         total_zeros +=m[_].count(0)
 
-
-# # # # # # # a = input('insert floor: ')
-
-# # # # # # # print(a=='')
-
-
-# # # # # a = 123123**0.5
-
-# # # # # print(a)
-
-# # # # # print(int(a))
-
-# # # # # print(round(a))
-
-
-# # # # a = 100000000
-
-# # # # print(len(str(a)))
-
-# # # a = 4
-
-# # # b = a**0.5
-
-# # # print(b)
-
-# # # print(type(b))
-
-# # # if type(b) == type(int()):
-# # #     print('int')
-# # # else:
-# # #     print('float')
-
-# # i = 5
-
-# # print(i == int(i**0.5)**2)
-
-# print((13%2, 13%3, 13%5, 13%7) == (1, 1, 3, 6))
-
-# # a, b, c, d = 13%2,3,5,7
-
-# # print(a,b,c,d == 0)
-
-def prime(n, primes):
-    if n<=1:
-        return True
-    elif n in (2,3,5,7):
-        return False
-    else:
-        if 0 in {n%2, n%3, n%5, n%7}:
-            return True
-        else:
-            sqrt_n = n**0.5
-            if n == int(sqrt_n)**2:
-                return True
-            else:
-                for i in primes:
-                    if i > (int(sqrt_n)+1):
-                        return False
-                    if n%i == 0:
-                        return True
-
-
-
-            
-#             ### old
-#             for i in range(2,int(n**0.5)+1):
-#                 # print(i)
-#                 if n%i==0:
-#                     return True
-
-# a = 13
-
-# print(0 in {a%2, a%3, a%5, a%7})
-
-# a = []
-
-# for i in a:
-#     print(i)
-def run():
-    """
-    docstring
-    """
-    primes = []
-    top = int(99194853094755497**0.5)+1 ## 314'952'144.13424065 aprox 22M primes <
-    for i in range(1,top):
-        if prime(i, primes) != True:
-            primes.append(i)
-            print(len(primes))
-    with open("file.txt", "w") as output:
-        output.write(str(primes))
-    # print(len(primes))
-    # percentage = len(primes)*100/top
-    
-    # print(f"{percentage}%")
-
-
-if __name__ == "__main__":
-    run()
-
-# print(3 not in range(2,4))
-
-# a = 9
-# print(a)
-# b = a**0.5
-# print(b)
-# c=int(b)**2
-# print(c)
-# print(c == a)
-
-# a = []
-
-# print(a[-1])
-
-# def aaaa(a):
-#     while True:
-#         if a > 1:
-#             return True
-#         else:
-#             return False
-
-# print(aaaa(2))
-
-
-# while True:
-#     for i in range(1,10):
-#         if i == 7:
-#             print(i)
-#             break
-#         else:
-#             print(f'{i} is not 7')
-#     print('in while')
+#     m = np.array(m)
+#     print(m)
+#     print(total_zeros)
+#     print(total_zeros/(sudoku_size*sudoku_size))
     
 
-# a = 7
 
-# print(list(range(a,a**2)))
+# random_matrix(2, 9)
+
+
+
+
+
+obj = {'ls{}'.format(i):random.sample([0, *range(1,sudoku_size+1)], counts = c, k = sudoku_size) for i in range(1,sudoku_size+1)}
+
+print(obj['ls1'])
+print(obj['ls1'].count(0))
+print(obj)
