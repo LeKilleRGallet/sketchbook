83045241085d2ba1560f135b10bc72b13bb33e23
LeKilleRGallet <arico@unal.edu.co>
2022-01-31 21:43:11 -0500

    Updated: 31/01/2022

--- Sketch.c
+++ Sketch.c
@@ -1,81 +1,22 @@
 #include <stdio.h>
 
 
-int read(int *array, int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &array[i]);
-    }
-}
+int sum(int& a, int& b) {
 
-int duplicates(int *original_array, int *tpm_array, int array_size)
-{
-    int counter = 0;
-    for (int i = 0; i < array_size; i++)
-    {
-        if(i == 0){
-            tpm_array[i] = original_array[i];
-        }
-        else{
-            for (int j = 0; j < i; j++)
-            {
-                if(original_array[i] == tpm_array[j]){
-                    counter++;
-                    break;
-                }
-                else if(j == i-1){
-                    tpm_array[i-counter] = original_array[i];
-                }
-            }
-        }
-    }
-    return array_size-counter;
-
-}
-
-int arraysize(int *array, int array_size){
-    int new_size = 0;
-    for (int i = 0; i < array_size; i++)
-    {
-        if (array[i] != 0)
-        {
-            new_size++;
-        }
-    }
-    return new_size;
-}
-
-int arrayprint(int *array, int array_size){
-    for (int i = 0; i < array_size; i++)
-    {
-        printf("%d\t", array[i]);
-    }
+    a = a+1;
+    b = b+1;
+    return a + b;
 }
 
 int main(){
-
+    int i;
     int n;
-    scanf("%d",&n);
-
-    //initialize a array with size n
-    int a[n];
-    //scan n values for the array
-    read(&a, n);
-
-    int tmp[n];
-
-    int size = duplicates(&a, &tmp, n);
-
-    int b[size];
-    for (int i = 0; i < size; i++)
-    {
-        b[i] = tmp[i];
-    }
-
-
-    arrayprint(&b, size);
-
+    printf("n: ");
+    scanf("%d", &n);
+    printf("\ni: ");
+    scanf("%d", &i);
+    int sumx = sum(n, i);
+    printf("n: %d\ni: %d\nsum: %d", n, i, sumx);
 
     return 0;
 }
