46ed5877d053723114e28bcf5427f40a0cffeb17
LeKilleRGallet <arico@unal.edu.co>
2022-02-23 22:23:15 -0500

    Updated: 23/02/2022

--- Sketch.py
+++ Sketch.py
@@ -1,16 +1,13 @@
-import time
+import random
 
-def loading():
-    for i in range(30):
-        if i%4==0:
-            r='/'
-        elif i%4==1:
-            r='—'
-        elif i%4==2:
-            r='\\'
-        elif i%4==3:
-            r='|'
-        print('loading...' + r,end='\r')
-        time.sleep(0.1)
 
-loading()
+prob=0.5
+arr1=[0,1]
+weights=[1-prob,prob]
+sum_a=0
+for _ in range(3):
+    a=random.choices(arr1,weights,k=1)
+    sum_a+=a[0]
+print(sum_a)
+
+
