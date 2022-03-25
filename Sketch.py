b309e7657128b1a91b299d474d3b6c85a2b5f4c3
LeKilleRGallet <arico@unal.edu.co>
2022-03-24 23:11:45 -0500

    Updated: 24/03/2022

--- Sketch.py
+++ Sketch.py
@@ -1,43 +1,44 @@
-white=0
-red=1
-green=2
-blue=3
-yellow=4
-orange=5
-
-white_face=[[white,white,white],[white,white,white],[white,white,white]]
-red_face=[[red,red,red],[red,red,red],[red,red,red]]
-green_face=[[green,green,green],[green,green,green],[green,green,green]]
-blue_face=[[blue,blue,blue],[blue,blue,blue],[blue,blue,blue]]
-yellow_face=[[yellow,yellow,yellow],[yellow,yellow,yellow],[yellow,yellow,yellow]]
-orange_face=[[orange,orange,orange],[orange,orange,orange],[orange,orange,orange]]
-
-mid_face=[red_face,green_face,blue_face,yellow_face]
-
-for i in range(3):
-    print('\t', end = '')
-    for j in range(3):
-        print(white_face[i][j],end=" ")
-    print()
-
-print('-'*30)
 
 
-j = 0
-while j < 3:
-    i=0
-    while i < 4:
-        
-        for k in range(3):
-            print(mid_face[i][j][k],end=" ")
-        print('|', end = '')
-        i += 1
-    print()
-    j += 1
-print('-'*30)
 
-for i in range(3):
-    print('\t', end = '')
-    for j in range(3):
-        print(orange_face[i][j],end=" ")
-    print()
+top_face=[[0,0,0],[0,0,0],[0,0,0]]
+mid1_face=[[1,1,1],[1,1,1],[1,1,1]]
+mid2_face=[[2,2,2],[2,2,2],[2,2,2]]
+mid3_face=[[3,3,3],[3,3,3],[3,3,3]]
+mid4_face=[[4,4,4],[4,4,4],[4,4,4]]
+bottom_face=[[5,5,5],[5,5,5],[5,5,5]]
+
+
+print(mid1_face)
+print(mid2_face)
+print(mid3_face)
+print(mid4_face)
+
+temp2=mid1_face[0]
+
+temp1=temp2
+temp2=mid2_face[0]
+mid2_face[0]=temp1
+
+temp1=temp2
+temp2=mid3_face[0]
+mid3_face[0]=temp1
+
+temp1=temp2
+temp2=mid4_face[0]
+mid4_face[0]=temp1
+
+temp1=temp2
+temp2=mid1_face[0]
+mid1_face[0]=temp1
+
+print()
+
+print(mid1_face)
+print(mid2_face)
+print(mid3_face)
+print(mid4_face)
+
+
+
+
