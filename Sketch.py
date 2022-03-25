46e5252d65b479b77e5bbdade32c671e38805d8c
LeKilleRGallet <arico@unal.edu.co>
2022-03-25 18:18:29 -0500

    Updated: 25/03/2022

--- Sketch.py
+++ Sketch.py
@@ -1,43 +1,28 @@
+import numpy as np
+
+def tttt(v1,v2,v3,v4):
+    return v4, v1, v2,v3
 
 
+top_face=[[0,'a',0],[0,'b',0],[0,'c',0]]
+mid1_face=[[1,'d',1],[1,'e',1],[1,'f',1]]
+mid2_face=[[2,'g',2],[2,'h',2],[2,'i',2]]
+mid3_face=[[3,'j',3],[3,'k',3],[3,'l',3]]
+mid4_face=[[4,'m',4],[4,'n',4],[4,'o',4]]
+bottom_face=[[5,'p',5],[5,'q',5],[5,'s',5]]
 
-top_face=[[0,0,0],[0,0,0],[0,0,0]]
-mid1_face=[[1,1,1],[1,1,1],[1,1,1]]
-mid2_face=[[2,2,2],[2,2,2],[2,2,2]]
-mid3_face=[[3,3,3],[3,3,3],[3,3,3]]
-mid4_face=[[4,4,4],[4,4,4],[4,4,4]]
-bottom_face=[[5,5,5],[5,5,5],[5,5,5]]
+tempmtrx1=np.array(mid2_face)
+tempmtrx2=np.array(mid4_face)
 
+top_face[2],a,bottom_face[0],b=tttt(top_face[2],list(tempmtrx1.T[0]),bottom_face[0],list(tempmtrx2.T[2]))
 
-print(mid1_face)
+print(top_face[2],a,bottom_face[0],b)
+
+for _ in range(3):
+    mid2_face[_][0]=a[_]
+
 print(mid2_face)
-print(mid3_face)
-print(mid4_face)
 
-temp2=mid1_face[0]
-
-temp1=temp2
-temp2=mid2_face[0]
-mid2_face[0]=temp1
-
-temp1=temp2
-temp2=mid3_face[0]
-mid3_face[0]=temp1
-
-temp1=temp2
-temp2=mid4_face[0]
-mid4_face[0]=temp1
-
-temp1=temp2
-temp2=mid1_face[0]
-mid1_face[0]=temp1
-
-print()
-
-print(mid1_face)
-print(mid2_face)
-print(mid3_face)
-print(mid4_face)
 
 
 
